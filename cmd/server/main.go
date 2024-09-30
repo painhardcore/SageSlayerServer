f2f76ed3813fc494e029e41aae6c744b5afe3663
Andrey Yurchenkov <painhardcore@gmail.com>
2024-10-01 01:53:48 +0200

    feat: pprof files on exit
    Signed-off-by: Andrey Yurchenkov <painhardcore@gmail.com>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,13 +4,35 @@ import (
 	"flag"
 	"log"
 	"math"
+	"os"
+	"os/signal"
+	"runtime/pprof"
+	"syscall"
 
 	"github.com/painhardcore/SageSlayerServer/internal/server"
 )
 
 func main() {
 	addr := flag.String("addr", ":8000", "TCP address to listen on")
+	cpuProfile := flag.String("cpuProfile", "cpu.prof", "File to store CPU profile")
+	memProfile := flag.String("memProfile", "mem.prof", "File to store memory profile")
+	goroutineProfile := flag.String("goroutineProfile", "goroutine.prof", "File to store goroutine profile")
 	flag.Parse()
+
+	// Start CPU profiling
+	cpuProfFile, err := os.Create(*cpuProfile)
+	if err != nil {
+		log.Fatalf("could not create CPU profile: %v", err)
+	}
+	defer cpuProfFile.Close()
+	if err := pprof.StartCPUProfile(cpuProfFile); err != nil {
+		log.Fatalf("could not start CPU profile: %v", err)
+	}
+	defer pprof.StopCPUProfile()
+
+	// Handle termination signals to stop profiling gracefully
+	exitChan := make(chan os.Signal, 1)
+	signal.Notify(exitChan, syscall.SIGINT, syscall.SIGTERM)
 
 	// Define the difficulty function
 	difficultyFunc := func(requestCount float64) int {
@@ -31,7 +53,35 @@ func main() {
 
 	srv := server.NewServer(*addr, 60.0, difficultyFunc)
 
-	if err := srv.Start(); err != nil {
-		log.Fatalf("Server error: %v", err)
+	go func() {
+		if err := srv.Start(); err != nil {
+			log.Fatalf("Server error: %v", err)
+		}
+	}()
+
+	// Wait for termination signal
+	<-exitChan
+	log.Println("Shutting down server...")
+
+	// Stop CPU profiling (deferred)
+
+	// Write memory profile
+	memProfFile, err := os.Create(*memProfile)
+	if err != nil {
+		log.Fatalf("could not create memory profile: %v", err)
+	}
+	defer memProfFile.Close()
+	if err := pprof.WriteHeapProfile(memProfFile); err != nil {
+		log.Fatalf("could not write memory profile: %v", err)
+	}
+
+	// Write goroutine profile
+	goroutineProfFile, err := os.Create(*goroutineProfile)
+	if err != nil {
+		log.Fatalf("could not create goroutine profile: %v", err)
+	}
+	defer goroutineProfFile.Close()
+	if err := pprof.Lookup("goroutine").WriteTo(goroutineProfFile, 0); err != nil {
+		log.Fatalf("could not write goroutine profile: %v", err)
 	}
 }
